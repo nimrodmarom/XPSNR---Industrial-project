59677c1f137e82a823693f61c775c1ae091253b9
Marom <nimrod.marom@intel.com>
2022-05-10 09:52:46 +0300

    Changing ffmpeg command to run from docker and support XPSNR results, adding them to graphs. Also moving some of the functions in the script to 'functions_for_script.py'

--- PSNR_calculate.py
+++ PSNR_calculate.py
@@ -1,42 +1,21 @@
 import os
 import matplotlib.pyplot as plt
 from PIL import Image
-import numpy as np
-
-def move_videos_to_folders():
-    """ Move all videos to its folder. """
-    for file in os.listdir():
-        if file.endswith('.mp4') or file.endswith('.avi'):
-            folder_name = file.split('__')[0]
-            if not os.path.exists(folder_name):
-                os.mkdir(folder_name)
-            os.system("move {0} {1}".format(file, folder_name))
-        
-
-def count_videos():
-    """ Count the videos in the folder. """
-    count = 0
-    for folder in os.listdir():
-        os.chdir(folder)
-        files = os.listdir()
-        #delete from files all files that are not mp4
-        for file in files:
-            if not (file.endswith('.mp4') or file.endswith('.avi')):
-                files.remove(file)
-        count += len(files)
-        os.chdir('..')
-    print(count)
-        
+from functions_for_script import *
 
 def handle_video(video_name: str):
     """ Create for a video_name 2 folders - all_data and results """
-    #TODO: adding to reuslts the bit rate from the video ffmpeg
     os.chdir("{0}".format(video_name)) 
+    print(os.getcwd()) #TODO: Delete - helps for debugging
 
-    if not os.path.exists("all_data"):
-        os.mkdir("all_data")
-    if not os.path.exists("results"):
-        os.mkdir("results")
+    if not os.path.exists('all_data_psnr'):
+        os.mkdir('all_data_psnr')
+    if not os.path.exists('results_psnr'):
+        os.mkdir('results_psnr')
+    if not os.path.exists("all_data_xpsnr"):
+        os.mkdir("all_data_xpsnr")
+    if not os.path.exists("results_xpsnr"):
+        os.mkdir("results_xpsnr")
     directory_files = os.listdir()
     original = ''
     has_changed = True
@@ -52,16 +31,18 @@ def handle_video(video_name: str):
                 directory_files.remove(file)
                 has_changed = True
                 break
-    os.chdir("all_data")
     if len(directory_files) == 0:
-        os.chdir("..\\..")
+        os.chdir("..")
         return
+    current_folder = os.getcwd()
     for file in directory_files:
         # make directory all_data
         all_data_name = 'all_data_' + file.split('.')[0] + '.txt'
         result_name = 'results_' + file.split('.')[0] + '.txt'
-        os.system("ffmpeg.exe -i ..\\{0} -i ..\\{1} -lavfi psnr=stats_file={2} -f null - > ..\\results\\{3} 2>&1".format(original, file, all_data_name, result_name))
-        os.chdir("..\\results")
+        os.system("docker run -v \"{0}:/data/orig\" -v \"{0}:/data/comp\" -v \"{0}\\all_data_psnr:/data/frame_out\" ffmpeg_docker:1_xpsnr -i \"/data/orig/{1}\" -i \"/data/comp/{2}\" -lavfi psnr=stats_file=\"/data/frame_out/{3}\" -f null - > results_psnr\\{4} 2>&1".format(current_folder, original, file , all_data_name, result_name))
+        os.system("docker run -v \"{0}:/data/orig\" -v \"{0}:/data/comp\" -v \"{0}\\all_data_xpsnr:/data/frame_out\" ffmpeg_docker:1_xpsnr -i \"/data/orig/{1}\" -i \"/data/comp/{2}\" -lavfi xpsnr=stats_file=\"/data/frame_out/{3}\" -f null - > results_xpsnr\\{4} 2>&1".format(current_folder, original, file , all_data_name, result_name))
+        os.chdir("results_psnr")
+        # change psnr files
         psnr_averge_value = 0
         bit_rate_value = 0
         with open(result_name, 'r') as result_file:
@@ -96,11 +77,46 @@ def handle_video(video_name: str):
         new_result_name = result_name[0 : last_index] + '__{0}.txt'.format(bit_rate_value)
         if (os.path.exists(new_result_name)):
             os.remove(new_result_name)
-        os.rename(result_name, new_result_name)
+        os.rename(result_name, new_result_name)  
+        # change XPSNR files      
 
-        os.chdir("..\\all_data")
+        os.chdir("..\\results_xpsnr")
+        xpsnr_averge_value = 0
+        bit_rate_value = 0
+        with open(result_name, 'r') as result_file:
+            # find the 2nd line which start with word "Duration"
+            bit_rate_line = ""                
+            found_first = False
+            # copy result file to result_file_temp
+            result_file_temp = result_file.readlines()
+            for line in result_file_temp:
+                line = line.strip()
+                if line.startswith("Duration"):
+                    if found_first:
+                        bit_rate_line = line
+                        break
+                    else:
+                        found_first = True
+            last_line = result_file_temp[-1].split(' ')
+            
+            xpsnr_averge_value = calculate_average_of_all_frames(file_name=all_data_name)
+            bit_rate_line = bit_rate_line.split(', ')
+            for i in range(len(bit_rate_line)):
+                if bit_rate_line[i].split(': ')[0] == 'bitrate':
+                    bit_rate_value = bit_rate_line[i].split(': ')[1].split(' ')[0]
+                    break
+        with open(result_name, 'w') as result:
+            result.write("bitrate: {0}".format(bit_rate_value))
+            result.write("\nXPSNR average: {0}".format(xpsnr_averge_value))
+        last_index = result_name.index('.')
+        new_result_name = result_name[0 : last_index] + '__{0}.txt'.format(bit_rate_value)
+        if (os.path.exists(new_result_name)):
+            os.remove(new_result_name)
+        os.rename(result_name, new_result_name)  
 
-    os.chdir("..\\..")
+        os.chdir("..")
+
+    os.chdir("..")
 
 def create_graph(video_name: str, different_codecs: str):
     """ create graph for video_name"""
@@ -109,7 +125,7 @@ def create_graph(video_name: str, different_codecs: str):
     # remove graph if exists
     if os.path.exists('{0}_graph.png'.format(video_name)):
         os.remove('{0}_graph.png'.format(video_name))
-    os.chdir("results")
+    os.chdir("results_psnr")
     
     all_codecs = []
     legend_title = 'Different codecs'
@@ -158,6 +174,37 @@ def create_graph(video_name: str, different_codecs: str):
             c_x.append(x_value)
             c_y.append(y_value)
     
+    os.chdir ("..\\results_xpsnr")
+    directory_files = os.listdir()
+    directory_files.sort(key=lambda x: float(x.split('__')[-1].split('.')[0]))
+    a_x_XPSNR, a_y_XPSNR = [], []
+    b_x_XPSNR, b_y_XPSNR = [], []
+    c_x_XPSNR, c_y_XPSNR = [], []
+    for file in directory_files:
+        file_attributes = file.split('__')
+        if len(file_attributes) == 6:
+            continue
+        if different_codecs:
+            current_codec = file.split('__')[6] 
+        else:
+            current_codec = file.split('__')[7]
+        x_value = 0
+        y_value = 0
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            x_value_line = lines[0].strip()
+            y_value_line = lines[1].strip()
+            x_value = float(x_value_line.split(': ')[-1])
+            y_value = float( y_value_line.split(': ')[-1])
+        if current_codec == all_codecs[0]:
+            a_x_XPSNR.append(x_value)
+            a_y_XPSNR.append(y_value)
+        if len(all_codecs) >= 2 and current_codec == all_codecs[1]:
+            b_x_XPSNR.append(x_value)
+            b_y_XPSNR.append(y_value)
+        if len(all_codecs) >= 3 and current_codec == all_codecs[2]:
+            c_x_XPSNR.append(x_value)
+            c_y_XPSNR.append(y_value)
     plt.figure()
 
     plt.title('{0}'.format(video_name))
@@ -165,21 +212,31 @@ def create_graph(video_name: str, different_codecs: str):
     plt.ylabel('PSNR value')
 
     if len(all_codecs) >= 1:
-        plt.plot(a_x, a_y, label = "{0} (base line)".format(all_codecs[0]), color='green', linestyle='solid' , linewidth = 3,
+        plt.plot(a_x, a_y, label = "PSNR {0} (base)".format(all_codecs[0]), color='green', linestyle='solid' , linewidth = 3,
              marker='o', markerfacecolor='red', markersize=6)
-        
+        plt.plot(a_x_XPSNR, a_y_XPSNR, label = "XPSNR {0} (base)".format(all_codecs[0]), color='green', linestyle='dashed' , linewidth = 3,
+                marker='o', markerfacecolor='red', markersize=6)
     if len(all_codecs) >= 2:
-        bd_rate = calculate_BD_rate(a_x, a_y, b_x, b_y)
-        bd_rate = round(bd_rate, 3)
-        plt.plot(b_x, b_y, label = "{0} BD-rate: {1}".format(all_codecs[1], bd_rate), color='blue', linestyle='solid', linewidth = 3,
+        bd_rate_psnr = calculate_BD_rate(a_x, a_y, b_x, b_y)
+        bd_rate_psnr = round(bd_rate_psnr, 1)
+        bd_rate_xpsnr = calculate_BD_rate(a_x_XPSNR, a_y_XPSNR, b_x_XPSNR, b_y_XPSNR)
+        bd_rate_xpsnr = round(bd_rate_xpsnr, 1)
+        
+        plt.plot(b_x, b_y, label = "PSNR {0} BD-rate: {1}".format(all_codecs[1], bd_rate_psnr), color='blue', linestyle='solid', linewidth = 3,
+            marker='o', markerfacecolor='red', markersize=6)
+        plt.plot(b_x_XPSNR, b_y_XPSNR, label = "XPSNR {0} BD-rate: {1}".format(all_codecs[1], bd_rate_xpsnr), color='blue', linestyle='dashed', linewidth = 3,
             marker='o', markerfacecolor='red', markersize=6)
         
     if len(all_codecs) >= 3:
-        bd_rate = calculate_BD_rate(a_x, a_y, c_x, c_y)
-        bd_rate = round(bd_rate, 3)
+        bd_rate_psnr = calculate_BD_rate(a_x, a_y, c_x, c_y)
+        bd_rate_psnr = round(bd_rate_psnr, 1)
+        bd_rate_xpsnr = calculate_BD_rate(a_x_XPSNR, a_y_XPSNR, c_x_XPSNR, c_y_XPSNR)
+        bd_rate_xpsnr = round(bd_rate_xpsnr, 1)
 
-        plt.plot(c_x, c_y, label = "{0} BD-rate: {1}".format(all_codecs[2], bd_rate), color='yellow', linestyle='solid', linewidth = 3,
+        plt.plot(c_x, c_y, label = "PSNR {0} BD-rate: {1}".format(all_codecs[2], bd_rate_psnr), color='yellow', linestyle='solid', linewidth = 3,
              marker='o', markerfacecolor='red', markersize=6)
+        plt.plot(c_x_XPSNR, c_y_XPSNR, label = "XPSNR {0} BD-rate: {1}".format(all_codecs[2], bd_rate_xpsnr), color='yellow', linestyle='dashed', linewidth = 3,
+                marker='o', markerfacecolor='red', markersize=6)
     
     if len(all_codecs) != 0:
         plt.legend(title="{0}".format(legend_title))
@@ -191,33 +248,6 @@ def create_graph(video_name: str, different_codecs: str):
     os.chdir('..')
 
 
-def calculate_BD_rate(R1, PSNR1, R2, PSNR2):
-    lR1 = np.log(R1)
-    lR2 = np.log(R2)
-
-    # rate method: sames as previous one but with inverse order
-    p1 = np.polyfit(PSNR1, lR1, 3)
-    p2 = np.polyfit(PSNR2, lR2, 3)
-
-    # integration interval
-    min_int = max(min(PSNR1), min(PSNR2))
-    max_int = min(max(PSNR1), max(PSNR2))
-
-    # indefinite interval of both polynomial curves
-    p_int1 = np.polyint(p1)
-    p_int2 = np.polyint(p2)
-
-    # evaluates both poly curves at the limits of the integration interval
-    # to find the area
-    int1 = np.polyval(p_int1, max_int) - np.polyval(p_int1, min_int)
-    int2 = np.polyval(p_int2, max_int) - np.polyval(p_int2, min_int)
-
-    # find avg diff between the areas to obtain the final measure
-    avg_exp_diff = (int2-int1)/(max_int-min_int)
-    avg_diff = (np.exp(avg_exp_diff)-1)*100
-
-    return avg_diff
-
 
 def main():
     os.chdir("..\\videos")
@@ -225,10 +255,11 @@ def main():
     # count_videos()
     
     ''' Produce the database: in each video folder'''
-    for folder_name in os.listdir():
-        if not os.path.isdir(folder_name):
-            continue
-        handle_video(folder_name)
+    # for folder_name in os.listdir():
+    #     if not os.path.isdir(folder_name):
+    #         continue
+
+    #     handle_video(folder_name)
 
     ''' Produce the graphs for each video: '''
     for folder_name in os.listdir():
---
+++ functions_for_script.py
@@ -0,0 +1,110 @@
+import os
+import numpy as np
+
+
+def move_videos_to_folders():
+    """ Move all videos to its folder. """
+    for file in os.listdir():
+        # if file is folder continue
+        if os.path.isdir(file):
+            continue
+        if file.endswith('.mp4') or file.endswith('.avi'):
+            folder_name = file.split('__')[0]
+            if not os.path.exists(folder_name):
+                os.mkdir(folder_name)
+            os.system("move {0} {1}".format(file, folder_name))
+        
+
+def count_videos():
+    """ Count the videos in the folder. """
+    count = 0
+    for folder in os.listdir():
+        os.chdir(folder)
+        files = os.listdir()
+        #delete from files all files that are not mp4
+        for file in files:
+            if not (file.endswith('.mp4') or file.endswith('.avi')):
+                files.remove(file)
+        count += len(files)
+        os.chdir('..')
+    print(count)
+        
+def calculate_average_of_all_frames(file_name) -> float:
+    print (os.getcwd())
+    os.chdir("..\\all_data_xpsnr")
+    count = 0
+    avg_sum = 0
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            # skip last line
+            if count == len(lines) - 2:
+                break
+            # make line to dictionary by key (": ") value
+            if len(line.split(": ")) >=2:
+                Y_value = float(line.split(": ")[2].split(" ")[0])
+                U_value = float(line.split(": ")[3].split(" ")[0])
+                V_value = float(line.split(": ")[4].split(" ")[0].strip())
+                
+                avg_frame = (Y_value + U_value + V_value) / 3
+                count += 1
+                avg_sum += avg_frame
+    os.chdir("..\\results_xpsnr")
+    return avg_sum/count
+
+def calculate_avg_by_folder(video_name):
+    print (os.getcwd())
+    os.chdir("..\\videos\\AC4BF\\all_data")
+    count = 0
+    sum_Y, sum_U, sum_V = 0, 0, 0
+    for file in os.listdir():
+        if file.endswith(".txt"):
+            with open(file, "r") as f:
+                lines = f.readlines()
+                for line in lines:
+                    # skip last line
+                    if count == len(lines) - 2:
+                        break
+                    # make line to dictionary by key (": ") value
+                    if len(line.split(": ")) >=2:
+                        Y_value = float(line.split(": ")[2].split(" ")[0])
+                        U_value = float(line.split(": ")[3].split(" ")[0])
+                        V_value = float(line.split(": ")[4].split(" ")[0])
+                        
+                        sum_Y += Y_value
+                        sum_U += U_value
+                        sum_V += V_value
+
+                        count += 1
+
+    print("AVG Y: ", sum_Y/count)
+    print("AVG U: ", sum_U/count)
+    print("AVG V: ", sum_V/count)
+
+
+def calculate_BD_rate(R1, PSNR1, R2, PSNR2):
+    lR1 = np.log(R1)
+    lR2 = np.log(R2)
+
+    # rate method: sames as previous one but with inverse order
+    p1 = np.polyfit(PSNR1, lR1, 3)
+    p2 = np.polyfit(PSNR2, lR2, 3)
+
+    # integration interval
+    min_int = max(min(PSNR1), min(PSNR2))
+    max_int = min(max(PSNR1), max(PSNR2))
+
+    # indefinite interval of both polynomial curves
+    p_int1 = np.polyint(p1)
+    p_int2 = np.polyint(p2)
+
+    # evaluates both poly curves at the limits of the integration interval
+    # to find the area
+    int1 = np.polyval(p_int1, max_int) - np.polyval(p_int1, min_int)
+    int2 = np.polyval(p_int2, max_int) - np.polyval(p_int2, min_int)
+
+    # find avg diff between the areas to obtain the final measure
+    avg_exp_diff = (int2-int1)/(max_int-min_int)
+    avg_diff = (np.exp(avg_exp_diff)-1)*100
+
+    return avg_diff
